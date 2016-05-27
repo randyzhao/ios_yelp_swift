357bc46571ad5c1bf1112fae5870328f06ad2a9d
Hongze Zhao <randy.zhao@airbnb.com>
2016-05-27 09:47:56 -0700

    Updated business cell style

--- Yelp/BusinessTableViewCell.swift
+++ Yelp/BusinessTableViewCell.swift
@@ -9,37 +9,38 @@
 import UIKit
 
 class BusinessTableViewCell: UITableViewCell {
-    var business: Business! {
-        didSet {
-            nameLabel.text = business.name
-            thumbImageView.setImageWithURL(business.imageURL!)
-            categoriesLabel.text = business.categories
-            addressLabel.text = "\(business.reviewCount!) Reviews"
-            ratingImageView.setImageWithURL(business.ratingImageURL!)
-            distanceLabel.text = business.distance
-        }
+  var business: Business! {
+    didSet {
+      nameLabel.text = business.name
+      thumbImageView.setImageWithURL(business.imageURL!)
+      categoriesLabel.text = business.categories
+      reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+      ratingImageView.setImageWithURL(business.ratingImageURL!)
+      distanceLabel.text = business.distance
+      addressLabel.text = "\(business.address!)"
     }
+  }
+  
+  @IBOutlet weak var thumbImageView: UIImageView!
+  @IBOutlet weak var addressLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var distanceLabel: UILabel!
+  @IBOutlet weak var ratingImageView: UIImageView!
+  @IBOutlet weak var reviewsCountLabel: UILabel!
+  @IBOutlet weak var categoriesLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
     
-    @IBOutlet weak var thumbImageView: UIImageView!
-    @IBOutlet weak var addressLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var distanceLabel: UILabel!
-    @IBOutlet weak var ratingImageView: UIImageView!
-    @IBOutlet weak var reviewsCountLabel: UILabel!
-    @IBOutlet weak var categoriesLabel: UILabel!
+    thumbImageView.layer.cornerRadius = 5
+    thumbImageView.clipsToBounds = true
+    // Initialization code
+  }
+  
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        thumbImageView.layer.cornerRadius = 5
-        thumbImageView.clipsToBounds = true
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
+    // Configure the view for the selected state
+  }
+  
 }
